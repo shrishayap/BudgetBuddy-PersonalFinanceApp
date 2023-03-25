14b08145496b3695095c04bc87e3b0a24cefa96b
Vihar Shah <qbq5nv@virginia.edu>
2023-03-25 15:06:42 -0400

    start of website

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'test'
+@app.route("/")
+def index():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/recommendations")
+def recommendations():
+    return render_template("recommendations.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
