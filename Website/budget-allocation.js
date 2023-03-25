5e29b9aba66d0ca7d7c1102db620bb1fd52f3342
Daniel Huynh <danielhuynh523@gmail.com>
2023-03-25 19:03:33 -0400

    fixed budget allocation functionality

--- Website/budget-allocation.js
+++ Website/budget-allocation.js
@@ -12,3 +12,18 @@ function updateTotalBudget() {
 bucketInputs.forEach(input => {
   input.addEventListener('input', updateTotalBudget);
 });
+
+function submittedBudget() {
+    let budget = {};
+    let categories = ["Education", "Food And Drink", "Health And Fitness", "Recreation", "Fashion And Beauty", "Entertainment", "Home And Vehicle", "Grocery", "Travel", "Miscellaneous"];
+    budget.totalBudget = document.getElementById("total-budget").value;
+    budget.buckets = {};
+
+    let i = 0;
+    document.querySelectorAll('input[name^="bucket"]').forEach(input => {
+        budget.buckets[categories[i]] = (parseFloat(input.value) || 0);
+        i++;
+    });
+    console.log(budget);
+    return budget;
+}
